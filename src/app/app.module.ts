59b8bb1e4432b6328a595bf64d3427aeb45a74e7
Ajay Singh <ajaysinghrr77@gmail.com>
2021-08-07 22:44:40 +0530

    Starting building

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule,NbCardModule, NbTabsetModule } from '@nebular/theme';
+import { MoviesComponent } from './movies/movies.component';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    BookmarksComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot([]),
+    BrowserModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbCardModule,
+    NbTabsetModule
   ],
   providers: [
 
---
+++ src/app/bookmarks/bookmarks.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bookmarks',
+  templateUrl: './bookmarks.component.html',
+  styleUrls: ['./bookmarks.component.css']
+})
+export class BookmarksComponent{
+
+}
